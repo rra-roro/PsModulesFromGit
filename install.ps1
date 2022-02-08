63aef40cfda863949614c4e10cdc37f28094554e
Rodion Rodionov <rra.roro@gmail.com>
2022-02-08 22:06:02 +0300

    Refactoring Install script

--- install.ps1
+++ install.ps1
@@ -1,91 +1,87 @@
+﻿<#
+  $url = 'https://my-gitlab/my-powershell/PsModulesFromGit/-/raw/main/install.ps1'
+
+  iex ("`$url='$url';"+(new-object net.webclient).DownloadString($url))
+#>
+
 # capture variable values
 [string]$Url = Get-Variable -ValueOnly -ErrorAction SilentlyContinue Url;
 
-#[string]$FileUrl = Get-Variable -ValueOnly -ErrorAction SilentlyContinue Url;
-
-
-[string]$User = Get-Variable -ValueOnly -ErrorAction SilentlyContinue User;
-[string]$Repo = Get-Variable -ValueOnly -ErrorAction SilentlyContinue Repo;
-[string]$Branch = Get-Variable -ValueOnly -ErrorAction SilentlyContinue Branch;
-[string]$ModulePath = Get-Variable -ValueOnly -ErrorAction SilentlyContinue Module;
-
-#set default values for Branch and ModulePath variables
-
-If ( [string]::IsNullOrWhitespace($Branch) ) { $Branch = "master" }
-If ( [string]::IsNullOrWhitespace($ModulePath) ) { $ModulePath = "" }
-
-function Read-ParamMode {
-    $c_url = New-Object System.Management.Automation.Host.ChoiceDescription '&Url', 'Define module repo path via repo URl'
-    #$c_file_url = New-Object System.Management.Automation.Host.ChoiceDescription '&Url', 'Define powershell script file path via URl'
-    $c_param = New-Object System.Management.Automation.Host.ChoiceDescription '&Params', 'Define module repo path one by one (user/repo/path)'
-    $options = [System.Management.Automation.Host.ChoiceDescription[]]($c_url, $c_param)
-
-    $message = 'Select Module repo enter mode'
-    return $host.ui.PromptForChoice($null, $message, $options, 1)
+function GetGroupValue($match, [string]$group, [string]$default = "") 
+{
+    $val = $match.Groups[$group].Value
+    Write-Debug $val
+    if ($val) 
+    {
+        return $val
+    }
+    return $default
 }
 
-function Read-RepoInfo {
-    param (
-        [string]$User,
-        [string]$Repo,
-        [string]$ModulePath,
-        [string]$Branch
-    )
-    $p_user = New-Object System.Management.Automation.Host.FieldDescription 'User ['+$User+']'
-    $p_user.Label="Github repo user name"
-    $p_user.IsMandatory=$true
-    $p_user.SetParameterType([string])
-    $p_user.DefaultValue = $User
+function Convert-Url()
+{
+    param( [string]$Url )
     
-    $p_repo = New-Object System.Management.Automation.Host.FieldDescription 'Repository ['+$Repo+']'
-    $p_repo.HelpMessage="Github Repository"
-    $p_repo.SetParameterType([string])
-    $p_user.DefaultValue = $Repo
+    $githubUriRegex = "(?<Scheme>https://)(?<Host>[^/]+)/"
+    $githubMatch = [regex]::Match($Url, $githubUriRegex);
 
-    $p_path = New-Object System.Management.Automation.Host.FieldDescription 'RepoFolder (empty for root) [' +$ModulePath +']'
-    $p_path.Label="Repository Folder [Live empty for root repo folder]"
-    $p_path.HelpMessage="Repository Folder [Live empty for root repo folder]"
-    $p_path.SetParameterType([string])
-    $p_path.DefaultValue = $ModulePath
+    if( $(GetGroupValue $githubMatch "Host") -eq "github.com")
+    {
 
-    $p_branch = New-Object System.Management.Automation.Host.FieldDescription 'Branch ['+$Branch+']'
-    $p_branch.HelpMessage="Repository branch [Default:master]"
-    $p_branch.SetParameterType([string])
-    $p_branch.DefaultValue = $Branch
+        $githubUriRegex = "(?<Scheme>https://)(?<Host>github.com)/(?<User>[^/]*)/(?<Repo>[^/]*)(/tree/(?<Branch>[^/]*)(/(?<Folder>.*))?)?(/archive/(?<Branch>.*).zip)?";
 
+        $githubMatch = [regex]::Match($Url, $githubUriRegex);
 
-    $res = $host.ui.Prompt($null,"Github Module Repository",@($p_user, $p_repo, $p_path, $p_branch))
+        if ( ! $(GetGroupValue $githubMatch "Host") ) 
+        {
+            throw [System.ArgumentException] "Incorrect 'Host' value. The 'github.com' domain expected";
+            #Write-Error -Message "Incorrect 'Host' value. The 'github.com' domain expected" -Category InvalidArgument
+        }
+    
+        return @{ 
+            User = GetGroupValue $githubMatch "User"
+            Repo = GetGroupValue $githubMatch "Repo"
+            Branch = GetGroupValue $githubMatch "Branch" "master"
+            ModulePath = GetGroupValue $githubMatch "Folder"
+        }
+    }
+    else
+    {   # Инсталируемся не с github, значит с нашего внутреннего GitLab
+        # https://my-gitlab/my-powershell/PsModulesFromGit/-/archive/main/PsModulesFromGit-main.zip
 
-    If ( ! [string]::IsNullOrWhitespace($res[$p_user.Name]) ) { $res[$p_user.Name] } Else {$res[$p_user.Name]=$p_user.DefaultValue} 
-    If ( ! [string]::IsNullOrWhitespace($res[$p_repo.Name]) ) { $res[$p_repo.Name] } Else {$res[$p_repo.Name]=$p_repo.DefaultValue} 
-    If ( ! [string]::IsNullOrWhitespace($res[$p_path.Name]) ) { $res[$p_path.Name] } Else {$res[$p_path.Name]=$p_path.DefaultValue} 
-    If ( ! [string]::IsNullOrWhitespace($res[$p_branch.Name]) ) { $res[$p_branch.Name] } Else {$res[$p_branch.Name]=$p_branch.DefaultValue} 
+        $githubUriRegex = "(?<Scheme>https://)(?<Host>[^/]+)/(?<Group>[^/]+)/(?<Repo>[^/]+)/-/raw/(?<Branch>[^/]+)/(?<Script>[^/]+)"
 
-    return @{
-        User = $res[$p_user.Name]
-        Repo = $res[$p_repo.Name]
-        Branch = $res[$p_branch.Name]
-        ModulePath = $res[$p_path.Name]
+        $githubMatch = [regex]::Match($Url, $githubUriRegex);
+
+        return @{ 
+            SchemeHost = $(GetGroupValue $githubMatch "Scheme") + $(GetGroupValue $githubMatch "Host")
+            Host = GetGroupValue $githubMatch "Host"
+            Group = GetGroupValue $githubMatch "Group"
+            Repo = GetGroupValue $githubMatch "Repo"
+            ModuleName = GetGroupValue $githubMatch "Repo"
+            Branch = GetGroupValue $githubMatch "Branch" "main"
+        }
+
     }
 }
 
-function Get-LocalTempPath {
-    param (
-        [string] $RepoName
-    )
+function Get-LocalTempPath 
+{
+    param ( [string] $RepoName )
+
     $tmpDir = [System.IO.Path]::GetTempPath();
     return "$tmpDir\$RepoName";
 }
 
-function Get-ModuleInstallFolder {
-    param (
-        [string] $ModuleName
-    )
+function Get-ModuleInstallFolder 
+{
+    param ( [string] $ModuleName )
 
     $separator = [IO.Path]::PathSeparator;
 
     $ProfileModulePath = $env:PSModulePath.Split($separator)[0];
-    if (!(Test-Path $ProfileModulePath)) {
+    if (!(Test-Path $ProfileModulePath)) 
+    {
         New-Item -ItemType Directory -Path $ProfileModulePath;
     }
 
@@ -100,37 +96,38 @@ function Get-ModuleInstallFolder {
     return $pathToInstal;
 }
 
-function Receive-Module {
+function Receive-Module 
+{
     param (
-        [string] $File,
-        [string] $Url
+        [string] $Url,
+        [string] $ToFile
     )
+
     $client = New-Object System.Net.WebClient;
     
-    try {
-
+    try
+    {
         $progressEventArgs = @{
-            InputObject = $client
-            EventName = 'DownloadProgressChanged'
-            SourceIdentifier = 'ModuleDownload'
-            Action = {
-                
-                Write-Progress -Activity "Module Installation" -Status `
-                ("Downloading Module: {0} of {1}" -f $eventargs.BytesReceived, $eventargs.TotalBytesToReceive) `
-                -PercentComplete $eventargs.ProgressPercentage 
-            }
-        };
+                                InputObject = $client
+                                EventName = 'DownloadProgressChanged'
+                                SourceIdentifier = 'ModuleDownload'
+                                Action = {
+                                            Write-Progress -Activity "Module Installation" -Status `
+                                                            ("Downloading Module: {0} of {1}" -f $eventargs.BytesReceived, $eventargs.TotalBytesToReceive) `
+                                                           -PercentComplete $eventargs.ProgressPercentage 
+                                         }
+                               };
 
         $completeEventArgs = @{
-            InputObject = $client
-            EventName = 'DownloadFileCompleted'
-            SourceIdentifier = 'ModuleDownloadCompleted'
-        };
+                                InputObject = $client
+                                EventName = 'DownloadFileCompleted'
+                                SourceIdentifier = 'ModuleDownloadCompleted'
+                              };
 
         Register-ObjectEvent @progressEventArgs;
         Register-ObjectEvent @completeEventArgs;
     
-        $client.DownloadFileAsync($Url, $File);
+        $client.DownloadFileAsync($Url, $ToFile);
 
         Wait-Event -SourceIdentifier ModuleDownloadCompleted;
     }
@@ -138,22 +135,24 @@ function Receive-Module {
     {  
         Write-Host("Cannot download $Url");
     } 
-    finally {
+    finally 
+    {
         $client.dispose();
         Unregister-Event -SourceIdentifier ModuleDownload;
         Unregister-Event -SourceIdentifier ModuleDownloadCompleted;
     }
-    Write-Debug "Unblock downloaded file access $File";
-    Unblock-File -Path $File;
+
+    Write-Debug "Unblock downloaded file access $ToFile";
+    Unblock-File -Path $ToFile;
 }
 
-function Expand-ModuleZip {
-    param (
-        [string] $Archive
-    )
+function Expand-ModuleZip 
+{
+    param ( [string] $Archive )
 
     #avoid errors on already existing file
-    try {
+    try 
+    {
 
         Write-Progress -Activity "Module Installation"  -Status "Unpack Module" -PercentComplete 0;
         
@@ -166,19 +165,33 @@ function Expand-ModuleZip {
     Write-Progress -Activity "Module Installation"  -Status "Unpack Module" -PercentComplete 40;
 }
 
-function Move-ModuleFiles {
+<#
+    https://my-gitlab/my-powershell/PsModulesFromGit/-/archive/main/PsModulesFromGit-main.zip
+#>
+
+function Move-ModuleFiles 
+{
     param (
         [string] $ArchiveFolder,
         [string] $Module,
         [string] $DestFolder,
         [string] $ModuleHash
     )
+    # Extracted zip module from GitHub 
     $path = (Resolve-Path -Path "${ArchiveFolder}\*-master\$Module").Path
+    if(!$path)
+    {
+        # Extracted zip module from GitLab
+        $path=(Resolve-Path -Path "${ArchiveFolder}\*-main\").Path
+    }
+
     Write-Progress -Activity "Module Installation"  -Status "Store computed moduel hash" -PercentComplete 40;
     Out-File -InputObject $ModuleHash -FilePath "$path\hash" 
     
     Write-Progress -Activity "Module Installation"  -Status "Copy Module to PowershellModules folder" -PercentComplete 50;
-    Move-Item -Path $path -Destination "$DestFolder";
+    Move-Item -Path $path -Destination "$DestFolder"
+    Remove-Item "$DestFolder\.gitattributes" -ErrorAction SilentlyContinue;
+    Remove-Item "$DestFolder\.gitignore" -ErrorAction SilentlyContinue;
     Write-Progress -Activity "Module Installation"  -Status "Copy Module to PowershellModules folder" -PercentComplete 60;
 }
 
@@ -203,111 +216,44 @@ function Write-Finish {
 }
 
 
-
-function GetGroupValue($match, [string]$group, [string]$default = "") {
-    $val = $match.Groups[$group].Value
-    Write-Debug $val
-    if ($val) {
-        return $val
-    }
-    return $default
-}
-
-function Convert-GitHubUrl(){
-    param (
-        [string]$Url
-    )
-    
-    $githubUriRegex = "(?<Scheme>https://)(?<Host>github.com)/(?<User>[^/]*)/(?<Repo>[^/]*)(/tree/(?<Branch>[^/]*)(/(?<Folder>.*))?)?(/archive/(?<Branch>.*).zip)?";
-
-    $githubMatch = [regex]::Match($Url, $githubUriRegex);
-
-    if ( ! $(GetGroupValue $githubMatch "Host") ) {
-        throw [System.ArgumentException] "Incorrect 'Host' value. The 'github.com' domain expected";
-        #Write-Error -Message "Incorrect 'Host' value. The 'github.com' domain expected" -Category InvalidArgument
-    }
-    return @{ 
-        User = GetGroupValue $githubMatch "User"
-        Repo = GetGroupValue $githubMatch "Repo"
-        Branch = GetGroupValue $githubMatch "Branch" "master"
-        ModulePath = GetGroupValue $githubMatch "Folder"
-    }
-}
-
-
-# in case when both Url and Repo variables are empty - request params in the interactive mode
-if ( "x$Url" -eq "x" -and "x$Repo" -eq "x" -and "x$FileUrl" -eq "x" ) {
-
-    $result = Read-ParamMode
-    switch ($result)
-    {
-        0 {
-            $Url = $host.ui.Prompt($null,$null,"Github Module Url")
-        }
-        1 { 
-            $res = Read-RepoInfo -User $User -Repo $Repo -ModulePath $ModulePath  -Branch $Branch
-            $User = $res['User'];
-            $Repo = $res['Repo'];
-            $Branch = $res['Branch'];
-            $ModulePath = $res['ModulePath'];
-         }
-        2 {
-            $FileUrl = $host.ui.Prompt($null,$null,"Github File Url")
-        }
-    }
-}
+$URLobj=@{}
 
 # try convert url to fully cvalified path
-if( -not [string]::IsNullOrWhitespace($Url) ){
-    $res = Convert-GitHubUrl -Url $Url
-    $User = $res['User'];
-    $Repo = $res['Repo'];
-    $Branch = $res['Branch'];
-    $ModulePath = $res['ModulePath'];
+if( -not [string]::IsNullOrWhitespace($Url) )
+{
+    $URLobj = Convert-Url -Url $Url
+}
+else
+{
+    throw [System.ArgumentException] "Incorrect `$Url variable with '$Url' value.";    
 }
 
 
-# try convert url to fully cvalified path
-if( -not [string]::IsNullOrWhitespace($Url) ){
-    $res = Convert-GitHubUrl -Url $Url
-    $User = $res['User'];
-    $Repo = $res['Repo'];
-    $Branch = $res['Branch'];
-    $ModulePath = $res['ModulePath'];
+$host.ui.WriteLine([ConsoleColor]::Green, [ConsoleColor]::Black, "Start downloading Module '$($URLobj['ModuleName'])' from $($URLobj['SchemeHost']) Repository:$($URLobj['Repo']) Branch:$($URLobj['Branch'])")
+
+$tmpArchiveName = $(Get-LocalTempPath -RepoName $URLobj['Repo']);
+$moduleFolder = Get-ModuleInstallFolder -ModuleName $URLobj['ModuleName'];
+
+$downloadUrl = ""
+
+if( $URLobj['Host'] -eq "github.com")
+{
+    $downloadUrl = [uri]"https://github.com/${User}/${Repo}/archive/${Branch}.zip";
+}
+else
+{
+    $downloadUrl = [uri]"$($URLobj['SchemeHost'])/$($URLobj['Group'])/$($URLobj['Repo'])/-/archive/$($URLobj['Branch'])/$($URLobj['ModuleName'])-$($URLobj['Branch']).zip"
 }
 
-if( -not ([string]::IsNullOrWhitespace($ModulePath)) ){
-    $moduleToLoad = $ModulePath;
-    $moduleName = Split-Path $moduleToLoad -leaf;
-    
-}
-else{
-    $moduleName = $Repo;
-    $moduleToLoad = "";
-}
+Receive-Module -Url $downloadUrl -ToFile "${tmpArchiveName}.zip";
 
-if( ([string]::IsNullOrWhitespace($Branch)) ){
-    $Branch = "master";
-}
+sleep 5
 
-$host.ui.WriteLine([ConsoleColor]::Green, [ConsoleColor]::Black, "Start downloading Module ${moduleName} from GitHub Repository:${Repo} User:${User} Branch:${Branch}")
+$moduleHash = Get-FileHash -Algorithm SHA384 -Path "${tmpArchiveName}.zip"
 
-$tempFile = Get-LocalTempPath -RepoName $Repo;
-$moduleFolder = Get-ModuleInstallFolder -ModuleName $moduleName;
+Expand-ModuleZip -Archive $tmpArchiveName;
 
-$downloadUrl = [uri]"https://github.com/${User}/${Repo}/archive/${Branch}.zip";
+Move-ModuleFiles -ArchiveFolder $tmpArchiveName -Module $URLobj['ModuleName'] -DestFolder $moduleFolder -ModuleHash "$($moduleHash.Hash)";
+Invoke-Cleanup -ArchiveFolder $tmpArchiveName
 
-$file =  "${tempFile}.zip";
-
-Receive-Module -Url $downloadUrl -File $file;
-
-$moduleHash = Get-FileHash -Algorithm SHA384 -Path $file
-
-$archiveName = $tempFile;
-
-Expand-ModuleZip -Archive $archiveName;
-
-Move-ModuleFiles -ArchiveFolder $archiveName -Module $moduleToLoad -DestFolder $moduleFolder -ModuleHash "$($moduleHash.Hash)";
-Invoke-Cleanup -ArchiveFolder $archiveName
-
-Write-Finish -moduleName $moduleName
+Write-Finish -moduleName $URLobj['ModuleName']
