f81e289350b1239f0316635b6836358f18e36ba4
Rodion Rodionov <rra.roro@gmail.com>
2023-04-09 00:11:23 +0200

    Remove old files

--- emulateDownload.ps1
+++
@@ -1,5 +0,0 @@
-$newstreamreader = New-Object System.IO.StreamReader(".\install.ps1")
-$in = $newstreamreader.ReadToEnd()
-$newstreamreader.Dispose()
-
-Invoke-Expression ('$module="Bookmarks";$user="stadub";$repo="PowershellScripts";'+$in)
